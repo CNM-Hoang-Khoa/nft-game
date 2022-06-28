e4edbffe0a48a40a7ff721b3c233889bbc19449a
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2022-06-28 23:59:28 +0700

    add fee & update fee method & require user to pay fee to mint a NFT

--- contracts/LipToken.sol
+++ contracts/LipToken.sol
@@ -5,7 +5,14 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LipToken is ERC721, Ownable {
+
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {}
+
     uint256 COUNTER;
+
+    uint256 fee = 0.01 ether;
 
     struct Lip {
       string name;
@@ -19,25 +26,35 @@ contract LipToken is ERC721, Ownable {
 
     event NewLip(address indexed owner, uint256 id, uint256 dna);
 
-    constructor(string memory _name, string memory _symbol)
-        ERC721(_name, _symbol)
-    {}
-
-    function _createLip(string memory _name, uint256 _dna) internal {
-      Lip memory newLip = Lip(_name, COUNTER, _dna, 1, 50);
+    function _createLip(string memory _name) internal {
+      uint8 randRarity = uint8(_createRandomNum(100));
+      uint256 randDna = _createRandomNum(10**16);
+      Lip memory newLip = Lip(_name, COUNTER, randDna, 1, randRarity);
       lips.push(newLip);
       _safeMint(msg.sender, COUNTER);
-      emit NewLip(msg.sender, COUNTER, _dna);
+      emit NewLip(msg.sender, COUNTER, randDna);
       COUNTER++;
     }
 
-    function _getRandomDna(string memory _str) internal pure returns(uint256) {
-      uint256 randomNum = uint256(keccak256(abi.encodePacked(_str)));
-      return randomNum % 10**16;
+    function _createRandomNum(uint256 _mod) internal view returns (uint256) {
+      uint256 randomNum = uint256(
+        keccak256(abi.encodePacked(block.timestamp, msg.sender))
+      );
+      return randomNum % _mod;
     }
-    function createRandomLip(string memory _name) public {
-      uint256 randDna = _getRandomDna(_name);
-      _createLip(_name, randDna);
+
+    function updateFee(uint256 _fee) external onlyOwner {
+      fee = _fee;
+    }
+
+    function withdraw() external payable onlyOwner {
+      address payable _owner = payable(owner());
+      _owner.transfer(address(this).balance);
+    }
+
+    function createRandomLip(string memory _name) public payable {
+      require(msg.value >= fee);
+      _createLip(_name);
     }
     
     function getLips() public view returns (Lip[] memory) {
