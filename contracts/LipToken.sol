b016f3c32fe4cc255438a1f27091336f9d8eb424
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2022-06-28 22:05:18 +0700

    try to create a mint method with auto-generated id

--- contracts/LipToken.sol
+++ contracts/LipToken.sol
@@ -5,7 +5,14 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LipToken is ERC721, Ownable {
-  constructor(string memory _name, string memory _symbol)
-    ERC721(_name, _symbol)
-  {}
+    uint256 COUNTER;
+
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {}
+
+    function mintMyNft(address toAddress) public onlyOwner(){
+        _safeMint(toAddress, COUNTER);
+        COUNTER++;
+    }
 }
