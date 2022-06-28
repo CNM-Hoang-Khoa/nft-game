54800bb3571139e90a0e1d26f72b294c2c982bc3
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2022-06-28 21:45:54 +0700

    Write LipToken contract based on openzeppelin

--- contracts/LipToken.sol
+++ contracts/LipToken.sol
@@ -3,3 +3,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LipToken is ERC721, Ownable {
+  constructor(string memory _name, string memory _symbol)
+    ERC721(_name, _symbol)
+  {}
+}
